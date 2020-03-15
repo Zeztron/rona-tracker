32ce54481e2d9715893b549d8574a4498a0947e6
unknown <pateltharsh@gmail.com>
2020-03-15 15:43:38 -0400

    added html template w/ thymeleaf

--- src/main/java/io/harshpatel/ronatracker/services/CoronaVirusDataService.java
+++ src/main/java/io/harshpatel/ronatracker/services/CoronaVirusDataService.java
@@ -23,6 +23,10 @@ public class CoronaVirusDataService {
 
     private List<LocationStats> allStats = new ArrayList<>();
 
+    public List<LocationStats> getAllStats() {
+        return allStats;
+    }
+
     @PostConstruct
     @Scheduled(cron = "* * 1 * * *") // Run every second
     public void fetchVirusData() throws IOException, InterruptedException {
@@ -41,7 +45,6 @@ public class CoronaVirusDataService {
             locationStat.setState(record.get("Province/State"));
             locationStat.setCountry(record.get("Country/Region"));
             locationStat.setLatestTotalCases(Integer.parseInt(record.get(record.size() - 1)));
-            System.out.println(locationStat);
             newStats.add(locationStat);
         }
         this.allStats = newStats;
