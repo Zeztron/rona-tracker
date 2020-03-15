4abfbc07f50d65f2b22db9f5f3541a1d0ca4558b
unknown <pateltharsh@gmail.com>
2020-03-15 14:23:15 -0400

    first commit

---
+++ src/main/java/io/harshpatel/ronatracker/RonaTrackerApplication.java
@@ -0,0 +1,13 @@
+package io.harshpatel.ronatracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RonaTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RonaTrackerApplication.class, args);
+	}
+
+}
